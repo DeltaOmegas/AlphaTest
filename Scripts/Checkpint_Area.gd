529fe8396871f64f7aadba8deca5b05170e727f2
NeonX <mail.haller.daniel@gmail.com>
2024-05-06 03:56:53 +0300

    Checkpoint rework
    Checkpoint IDLE and REACH animations were added

--- Scripts/Checkpint_Area.gd
+++ Scripts/Checkpint_Area.gd
@@ -3,8 +3,10 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Player:
-		body.Checkpoint($CollisionShape2D.position)
-		if get_collision_mask_value(3):
-			print("White checkpoint")
-		else:
-			print("Black checkpoint")
+		body.Checkpoint(position+Vector2(0, -1000))
+		$"../Checkpoint_Animation".stop()
+		$"../Checkpoint_Animation".play("Reached")
+		
+
+func _ready():
+		$"../Checkpoint_Animation".play("Idle")
