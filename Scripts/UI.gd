a92f0d1b5d87d641e1d4206a052460008b10167e
DeltaOmegas <gagarin20061@gmail.com>
2024-05-07 23:27:43 +0300

    minor changes

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,6 +1,13 @@
 extends CanvasLayer
 
-
-
+#func _ready():
+#	$Flashlight_Timer.visible(true)
+#	$Health_indicator.visible(true)
+	
+	
 func update_timer(time:String):
 	$Flashlight_Timer.set_text(time)
+	
+func update_health(health: int):
+	$Health_indicator.set_text(health)
+	
