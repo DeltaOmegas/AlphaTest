920ea6cccb579f592b0bca5c8774280551d6df60
NeonX <mail.haller.daniel@gmail.com>
2024-05-09 02:24:48 +0300

    Elevator system was added

--- Scripts/Elevator.gd
+++ Scripts/Elevator.gd
@@ -1,3 +1,7 @@
 extends Node2D
 
-@export var is_to_bottom = true
+
+@export var is_downward: bool = true
+@export_range(0.1, 30) var length: int = 1
+@export_range(1, 300) var lift_speed: int = 10
+@export_range(1, 300) var drop_speed: int = 10
--- Scripts/Elevator_Area.gd
+++ Scripts/Elevator_Area.gd
@@ -1,30 +1,36 @@
 extends Area2D
 
-@onready var is_to_bottom = $"../..".is_to_bottom
-@onready var is_up: bool = is_to_bottom
+@onready var direction: bool = $"../..".is_downward
+@onready var length: int = $"../..".length
+@onready var is_up: bool = not direction
+@onready var lift_speed: int = $"../..".lift_speed
+@onready var drop_speed: int = $"../..".drop_speed
+var tween
+
+
 
 func elevator_down():
-	if not($"../Elevator_Animation".is_playing()):
-		if is_up and is_to_bottom:
-			$"../Elevator_Animation".play("Fall_from_neutral")
-			is_up = false
-		elif is_up and not is_to_bottom:
-			$"../Elevator_Animation".play("Fall_from_top")
-			is_up = false
-		
+	if tween:
+		tween.kill()
+		tween = false
+	tween = $"..".create_tween()
+	if direction:
+		tween.tween_property($"..", "position", Vector2(0, 16*length-6), ((Vector2(0, 16*length-6)-$"..".position)/drop_speed).y)
+	else:
+		tween.tween_property($"..", "position", Vector2(0, 0), ($"..".position/drop_speed).y)
+
 
 func elevator_up():
-	if not($"../Elevator_Animation".is_playing()):
-		if not is_up and is_to_bottom:
-			$"../Elevator_Animation".play("Lift_from_bottom")
-			is_up = true
-		elif not is_up and not is_to_bottom:
-			$"../Elevator_Animation".play("Lift_from_neutral")
-			is_up = true
-
-func _ready():
-	$"..".position = Vector2(0,0)
-
+	if tween:
+		tween.kill()
+		tween = false
+	tween = $"..".create_tween()
+	if direction:
+		tween.tween_property($"..", "position", Vector2(0, 0), ($"..".position/lift_speed).y)
+	else:
+		tween.tween_property($"..", "position", Vector2(0, -16*length), ((Vector2(0, -16*length)-$"..".position)/lift_speed).y)
+		
+		
 func _on_body_entered(body):
 	if body is Player:
 		body.is_on_elevator = $"."
