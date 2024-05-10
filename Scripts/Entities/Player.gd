796beb1ab9671ab21896a3dd15bfc4e09a834c7b
NeonX <mail.haller.daniel@gmail.com>
2024-05-10 13:03:00 +0300

    Animations

--- Scripts/Entities/Player.gd
+++ Scripts/Entities/Player.gd
@@ -11,9 +11,11 @@ var _health: int = 8#STOP! DO NOT MODIFY _health WITHOUT set_health THIS CAN CAU
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var checkpoints: Array # 2d array e.g [[Vector2(position), checkpoint_color(true/false)
 var is_on_elevator
-var is_on_ladder 
 var forcepushed: Array = [0,0,false] #[reqd speed, iterator, is turned on]
 
+
+func animation(type):
+	pass
 func respawn(): #Used in Death_Zone to respawn player
 	position = checkpoints[-1][0]
 	velocity = Vector2(0, 0) #Fix the "portal effect"
@@ -31,6 +33,7 @@ func get_last_checkpoint_color(): #Used in Death_Zone to switch color if it is n
 	return checkpoints[-1][1]
 
 func force_jump(data): #force player to jump by a third force
+	$Animation.play("Jump")
 	if typeof(data) == 2:
 		velocity.y = data
 	elif typeof(data) == 4:
@@ -78,9 +81,8 @@ func _process(_delta):
 	if Input.is_action_just_pressed("up") and is_on_elevator != null:
 		is_on_elevator.elevator_up()
 
-
 func _physics_process(delta):
-	if not is_on_floor() and not is_on_ladder:
+	if not is_on_floor():
 		velocity.y += gravity * delta
 	elif not forcepushed[1]:
 		forcepushed = [0,0, false]
@@ -92,19 +94,18 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_height
-		
 	var direction = Input.get_axis("left", "right")
 	if direction and not forcepushed[2]:
 		if direction == -1:
-			$Sprite2D.set_flip_h(true)
+			$Animation.set_flip_h(false)
 		else:
-			$Sprite2D.set_flip_h(false)
+			$Animation.set_flip_h(true)
 		velocity.x = direction * speed
 	elif forcepushed[2]:
 		if forcepushed[0]:
 			velocity.x = forcepushed[0]
 	else:
 		velocity.x = 0
-		$AnimationPlayer.play("Idle")
 	
 	move_and_slide()
+	
